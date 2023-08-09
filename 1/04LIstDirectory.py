088e6dafc3ead31d9e798816faad7766b6458fb2
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-09 20:25:54 +0545

    Update

--- 1/04LIstDirectory.py
+++ 1/04LIstDirectory.py
@@ -1,2 +1,4 @@
 import os
+import sys
 print(os.listdir("/home/wabisabi/Desktop")) # Print the directory of desktop
+print(sys.version) # display the version of python
---
+++ 1/05UsingExternalModule.py
@@ -0,0 +1,2 @@
+from playsound import playsound
+playsound("/home/wabisabi/Desktop/Python/audio.mp3")
