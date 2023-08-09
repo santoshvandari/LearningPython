d5abc864fa647893997310cca77325728d056ca2
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-09 23:36:54 +0545

    Update

---
+++ 2/01Variables.py
@@ -0,0 +1,14 @@
+# Variables in Python
+a=56 #This is Integer
+b="Santosh Bhandari" # This is string
+c=254.64 # This is Float
+d=True #this is boolean
+e=None
+print(f"{a}\n{b}\n{c}\n{d}") #Printing all the Statement in Single Line.
+# Using the Multiple Line to Display Result
+print(f"Integer: {a}")
+print(f"String: {b}")
+print(f"Float: {c}")
+print(f"Boolean: {d}")
+print(f"Null: {e}")
+
