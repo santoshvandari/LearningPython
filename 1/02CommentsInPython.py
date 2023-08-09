aee0a3c703fd2ce37a499e5adf2ccaf8c455257f
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-09 16:04:16 +0545

    Initial Commit

---
+++ 1/02CommentsInPython.py
@@ -0,0 +1,11 @@
+print("I am Learning to Comment in Python")
+# Using Single Line Comment.
+# Single Line Commnet is Done by Using Python. 
+print("Python is Awesome. ") # Comment can also be used like this.
+print("Comments are Single and Multi Line. '#' is used in Single line Comment. \n ''' ''' is Used for Multiline Comment. ")
+'''
+I am Using Multi Line Comment in Python. 
+This is the Year of 2023 and I am currently Learning Python.
+Author: Santosh Bhandari
+License to: Self
+'''
