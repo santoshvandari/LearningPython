b942e84b2a5f5cfc016ffb6c7508385eeca244f4
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-10 11:46:53 +0545

    Update

---
+++ 2/04InputFunction.py
@@ -0,0 +1,8 @@
+# Using Python Input Function
+# Python take every input as String. 
+name=input("Enter Your Name: ")
+# age=input("Enter Your Age: ")
+# age=int(age)
+age=int(input("Enter Your Age: "))
+print("Your Name is: ",name," and Age: ",age)
+print("Type of Name: ",type(name),"\nType of Age: ",type(age))
