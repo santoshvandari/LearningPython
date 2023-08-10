a89fde91306395de0533bd874f521703b2732e5b
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-10 17:12:01 +0545

    Update

---
+++ 3/06ConcatInPy.py
@@ -0,0 +1,10 @@
+# user=input("Enter Your Name: ")
+# result="Good Afternoon "+user
+# print(result)
+
+name=input("Enter a Name: ")
+date=input("Enter a Date: ")
+letter = '''Dear  {name},
+                You are Selected!
+                {date}'''
+print(letter)
