b6b1b1bc597fdf7ef3b5ce185d61470b77a3469d
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-10 16:49:08 +0545

    Update

--- 3/02StringConcat.py
+++ 3/02StringConcat.py
@@ -3,3 +3,6 @@ b="Santosh"
 concat= a+b
 print(concat)
 print(b[4])
+print(b[0:3]) # take the character from 0 index to 2 index. means 0,1,2 char index
+print(b[-4:-1])  
+print(len(b)) # Length of variable
