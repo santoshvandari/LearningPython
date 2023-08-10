76b7a0962312ae26b4a19fe20ac5bab106ae5a59
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-10 11:43:38 +0545

    Update

---
+++ 2/03TypeCasting.py
@@ -0,0 +1,20 @@
+#Type Casting and printing the Type of variable
+
+a=10
+b="10"
+c=10.24
+print("Type of A: ",type(a), type(b),type(c))
+
+
+a="10"
+print(type(a))
+a=int(a)
+print(type(a))
+b=20
+print(type(b))
+b=str(20)
+print(type(b))
+c=float(a)
+print(type(c))
+print("A: ",a,"\nB: ",b,"\nC: ",c)
+
