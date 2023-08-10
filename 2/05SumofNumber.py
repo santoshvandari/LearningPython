2458644fc558e563fb2dc19f2cad99a3fa96cf0f
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-10 11:50:13 +0545

    Update

---
+++ 2/05SumofNumber.py
@@ -0,0 +1,4 @@
+# Taking Two Numbers from the users and Sum those numbers
+num1=int(input("Enter First Number: "))
+num2=int(input("Enter Second Number: "))
+print("Addition = ",num1+num2,"\nSubtraction = ",num1-num2,"\nMultiplication = ",num1*num2,"\nDivision = ",num1/num2)
