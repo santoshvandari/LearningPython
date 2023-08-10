099bd15e1873b391feca5064f032a0bcdf011cf2
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-10 11:37:12 +0545

    Update

--- 2/02Operators.py
+++ 2/02Operators.py
@@ -5,9 +5,20 @@
 4. Logical Operator: and, or , not
 '''
 #Arithmetic Operator
-a=10
-b=5
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
+# a=10
+# b=5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+#Assignment Operator
+# a=10
+# print(a)
+# a+=5
+# print(a)
+# a-=4
+# print(a)
+# a*=10
+# print(a)
+# print(a)
