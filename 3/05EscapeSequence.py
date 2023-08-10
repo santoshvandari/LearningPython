7ee506cabf144d0952d0930664cb82f069ed4da2
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-10 17:07:28 +0545

    Update

---
+++ 3/05EscapeSequence.py
@@ -0,0 +1,8 @@
+# \n Is used for new Line.
+# \t is used for tab
+# \' is used for single quotes.
+# \\ is used for blackslash
+# There are other escape character also 
+
+a="Hello, I\'m Santosh Bhandari.\nI am From Jhapa Nepal.\n\t Have A Good Day."
+print(a)
