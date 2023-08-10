2ccf3e5866a8fb426e068fb866277efee82c3e20
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-10 18:59:51 +0545

    Update

--- 4/01List.py
+++ 4/01List.py
@@ -2,3 +2,5 @@ l1=[1,5,8,10,2,3,58,26,24]
 print(l1)
 list=["Santosh",21,"Kishna",24]
 print(list)
+list[0]="Mahesh"
+print(list)
---
+++ 4/02ListSlice.py
@@ -0,0 +1,3 @@
+list=["Santosh",21,"Ayush",20,"Krishna",22]
+print(list[0:2])
+print(list[:-2])
