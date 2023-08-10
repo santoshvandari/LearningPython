eb58ae312d417e7df22ae80c1017fda5ad519a23
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-10 17:24:30 +0545

    Update

---
+++ 3/07SomePractice.py
@@ -0,0 +1,8 @@
+string = "This is the String with Double  Spaces."
+print(string.find("  "))
+print(string.replace("  "," "))
+
+letter="Dear Santosh,This Course is Nice. Thank you!"
+print(letter)
+formattedLetter="Dear Santosh,\n\tThis Course is Nice.\nThanks!"
+print(formattedLetter)
