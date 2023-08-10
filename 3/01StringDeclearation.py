a70d2669bce103d041301e016cae6e00c94f8edf
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-10 15:18:20 +0545

    Update

---
+++ 3/01StringDeclearation.py
@@ -0,0 +1,7 @@
+# There are Three Types of String Declearation in Python.
+# Here are the mentods
+
+str1='String 1' #Using Single Quotes
+str2="String 2" #Using Double Quotes
+str3 = '''String 3''' #Using Trible Single Quotes
+print(type(str1), type(str2), type(str3))
