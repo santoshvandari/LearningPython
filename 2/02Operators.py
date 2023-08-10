3caa41bbda5e0aa5709f8803de36bf04d847ad6d
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-10 11:36:05 +0545

    Update

--- 2/02Operators.py
+++ 2/02Operators.py
@@ -2,5 +2,12 @@
 1. Arithmetic Operator: +,-,*,/ etc.
 2. Assignment Operator: =,+=,-= etc.
 3. Comparison Operator: ==,>,>=,<,!= etc.
-4. Logical Operator: AND, OR , NOT
+4. Logical Operator: and, or , not
 '''
+#Arithmetic Operator
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
