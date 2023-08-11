c2c244ad380b1fa8f7d432f143acd65e6599be8a
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-11 10:38:03 +0545

    Update

---
+++ 4/05ListEnteredByUser.py
@@ -0,0 +1,10 @@
+# Store the Seven Name of Fruits in List Entered by USers.
+f1=input("Enter a Fruit 1: ")
+f2=input("Enter a Fruit 2: ")
+f3=input("Enter a Fruit 3: ")
+f4=input("Enter a Fruit 4: ")
+f5=input("Enter a Fruit 5: ")
+f6=input("Enter a Fruit 6: ")
+f7=input("Enter a Fruit 7: ")
+fruit=[f1,f2,f3,f4,f5,f6,f7]
+print(fruit)
