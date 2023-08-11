3a31c21183e1c964ac0f38d1d400a7ba2ad8bc20
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-11 15:33:22 +0545

    Update

---
+++ 5/01Dictionary.py
@@ -0,0 +1,12 @@
+Dictionary={
+    "name":"Santosh Bhandari",
+    "age": 21,
+    "address": "Jhapa",
+    "marks":[20,25,16,25]
+}
+print(Dictionary)
+print(Dictionary["name"])
+print(Dictionary["marks"])
+
+Dictionary["name"]="Krishna"
+print(Dictionary)
