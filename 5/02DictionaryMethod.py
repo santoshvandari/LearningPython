a8e2b25f5047d679208c8886394151b5ed4eb218
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-11 15:37:29 +0545

    Update

---
+++ 5/02DictionaryMethod.py
@@ -0,0 +1,16 @@
+myDict={
+    "name":"Santosh Bhandari",
+    "age": 21,
+    "address": "Jhapa",
+    "marks":[20,25,16,25]
+}
+
+print(myDict.items())
+print(myDict.keys())
+print(myDict.values())
+updateDict={
+    'College':"Mechi Multiple Campus",
+    "Level":"Bachelor",
+    "Semester":"Fourth"
+}
+myDict.update(updateDict)
