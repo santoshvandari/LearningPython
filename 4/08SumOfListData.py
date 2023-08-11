24b6f4da75a4bd72a1fe5d9751e63bbf4ebf9e78
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-11 11:44:29 +0545

    Update

--- 4/08SumOfListData.py
+++ 4/08SumOfListData.py
@@ -1,3 +1,4 @@
 list=[5,18,28,60]
-sum=list[0]+list[1]+list[2]+list[3]
-print("Sum of List Data: ",sum)
+sumList=list[0]+list[1]+list[2]+list[3]
+print("Sum of List Data: ",sumList)
+print("Sum of List Data: ",sum(list))
