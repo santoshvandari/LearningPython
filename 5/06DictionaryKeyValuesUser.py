b764e8e3e22af79196338761fde90f13cbee624d
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-11 15:57:32 +0545

    Update

---
+++ 5/06DictionaryKeyValuesUser.py
@@ -0,0 +1,10 @@
+n1=input("Enter Your Name: ")
+l1=input("Enter You Favourite Language: ")
+n2=input("Enter Your Name: ")
+l2=input("Enter You Favourite Language: ")
+
+FavLang={
+    n1:l1,
+    n2:l2
+}
+print(FavLang)
