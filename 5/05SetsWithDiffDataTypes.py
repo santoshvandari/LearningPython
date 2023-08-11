7d31daf62dd8d6a2638833a4e4d98cfd3c397dd9
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-11 15:55:15 +0545

    Update

---
+++ 5/05SetsWithDiffDataTypes.py
@@ -0,0 +1,16 @@
+# a={8,"8"}
+# print(a)
+
+
+# b=set()
+# d1=int(input("Enter a First Number: "))
+# d2=int(input("Enter a Second Number: "))
+# d3=int(input("Enter a Third Number: "))
+# d4=int(input("Enter a Fourth Number: "))
+# d5=int(input("Enter a Fifth Number: "))
+# b.add(d1)
+# b.add(d2)
+# b.add(d3)
+# b.add(d4)
+# b.add(d5)
+# print(b)
