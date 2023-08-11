b353eaed6be8006731095f76c0ffdc68789d24bb
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-11 15:49:41 +0545

    Update

--- 5/03Sets.py
+++ 5/03Sets.py
@@ -1,23 +1,10 @@
 # Creating the Sets
 a={1,2,3,4,5,6,80,10}
-# print(type(a))
-# print(a)
+print(type(a))
+print(a)
 
 # creating an empty sets
 b=set()
-# print(type(b))
-# print(b)
+print(type(b))
+print(b)
 
-b.add(5)
-b.add(1)
-b.add(10)
-b.add(("Santosh","Bhandari"))
-# print(b)
-# print(len(b))
-# b.remove(1)
-# print(b)
-print(b)
-print(b.pop())
-print(b)
-b.clear()
-print(b)
