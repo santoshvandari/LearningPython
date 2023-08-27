cb1b33f98a0f7ec45997b7a0907a9aeb293fb30d
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-27 20:49:41 +0545

    Initial

--- 6/08Filter.py
+++ 6/08Filter.py
@@ -1,6 +1,12 @@
-spam=['make a lot of money',"buy now",'subscribe this',"click this"]
-cmt=input("Enter Your Comment: ")
-if(cmt in spam):
-    print("Your Comment is Spam")
-else:
-    print("Your comment is Fine.")
+# spam=['make a lot of money',"buy now",'subscribe this',"click this"]
+# cmt=input("Enter Your Comment: ")
+# if(cmt in spam):
+#     print("Your Comment is Spam")
+# else:
+#     print("Your comment is Fine.")
+
+comment = input("Enter Your Comment: ")
+if("make a lot of money" or "buy now" or "click this" or "subscribe this" in comment):
+    print("Your Comment is Spam.")
+else: 
+    print("Your Comment is Fine")
