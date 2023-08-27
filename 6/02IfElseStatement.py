58f92cd168ca61eaaa65378082fc561d116e1c4e
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-27 20:00:32 +0545

    Initial

---
+++ 6/02IfElseStatement.py
@@ -0,0 +1,6 @@
+# Check Whether Eligible for Voting or Not.
+age=int(input("Enter Your Age: "))
+if(age>17):
+    print("You are Eligible for Voting.")
+else:
+    print("You are Not Eligible for Voting. ")
