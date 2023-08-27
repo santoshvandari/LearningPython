8666efea4163073f81c271466f100e1df4094f1d
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-27 20:46:29 +0545

    Initial

---
+++ 6/09usernameValidate.py
@@ -0,0 +1,5 @@
+username=input("Enter Your UserName: ")
+if(len(username)>=10):
+    print("Your Username is Fine and Contains More than Nine Characters.")
+else:
+    print("Invalid Username. Must Contains More than Nine Characters.")
