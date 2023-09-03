2060ea5bfe55780a1e2275787cdecd7e6b96e904
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-03 23:28:31 +0545

    Initial

---
+++ 8/03SumUsingWhile.py
@@ -0,0 +1,11 @@
+'''
+Sum the Numbers from 1 to 10 using while loop.
+'''
+i=1
+sum=0
+while i<=10:
+    sum=sum+i
+    print("Sum of 1 to ",str(i)," is ",sum,".")
+    i+=1
+    
+print("Sum of Numbers from 1 to 10 = ",sum)
