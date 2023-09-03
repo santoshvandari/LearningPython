44caf220bc86fcfd33580513ac519e2a5c0c2d9a
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-03 23:48:29 +0545

    Initial

---
+++ 8/10UsingBreak.py
@@ -0,0 +1,11 @@
+for i in range(1,101):
+    if(i==10):
+        break
+    print(i)
+
+else:
+    print("Numbers from 1 to 100 Printed.")
+
+'''
+else part will not execute if you use break keyword to get out of loop. 
+'''
