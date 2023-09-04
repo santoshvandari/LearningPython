151d544782df2869544089875500aca5af4fa2cb
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-04 21:18:57 +0545

    Initial

--- 9/04OddAndEven.py
+++ 9/04OddAndEven.py
@@ -2,13 +2,13 @@ def odd(n):
     if(n%2):
         print(n, " is a Odd Number.")
     else:
-        print(n," is a Even Number.")
+        print(n," is a not a Odd Number.")
 
 def even(n):
-    if(n%2==0):
-        print(n," is a Even Number.")
-    else:
+    if(n%2):
         print(n," is not a Even Number.")
-num=int(print("Enter a Number to Check Odd or Even: "))
+    else:
+        print(n," is a Even Number.")
+num=int(input("Enter a Number to Check Odd or Even: "))
 odd(num)
 even(num)
