7ecd0ba76b0c7f2a9f10ba63000f07b8472b3aa7
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-05 21:21:19 +0545

    Initial

---
+++ 8/19Pattern.py
@@ -0,0 +1,13 @@
+'''
+Print the FOllowing Patterns
+
+  *
+ ***
+*****
+
+'''
+for i in range(3):
+    for j in range(1,i+2):
+        print("*",end="")
+
+    print()
---
+++ 8/20Pattern.py
@@ -0,0 +1,10 @@
+'''
+*
+**
+***
+'''
+
+for i in range(3):
+    for j in range(i+1):
+        print("*",end="")
+    print()
