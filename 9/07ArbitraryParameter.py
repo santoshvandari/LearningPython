2b9813e6b22a4d0e4c32a8800f6f036d2f095682
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-06 20:19:04 +0545

    Initial

--- 9/07ArbitraryParameter.py
+++ 9/07ArbitraryParameter.py
@@ -1,10 +1,10 @@
 def Sum(*num):
     sum = 0
-    for i in range(num):
+    for i in num:
         sum+=i
     return sum
 
 print(Sum(1))
 print(Sum(5,6,8,10))
-print(Sum(1))
-print(Sum(1))
+print(Sum(2,6,9,10,22,59))
+print(Sum(15,6,5,10))
