2749d71cb80d9e1ab91ffef0c16d3a37080e24f5
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-07 22:48:46 +0545

    Initial

--- 9/08PrintUsingRecursion.py
+++ 9/08PrintUsingRecursion.py
@@ -1,9 +1,9 @@
 def Display(n):
     if(n==1):
-        return print("Hello")
-    print("Hello")
+        return print("Hello "+str(n))
+    print("Hello "+str(n))
     return Display(n-1)
 
 # Display(5)
-Display(2)
-# Display(10)
+# Display(2)
+Display(10)
