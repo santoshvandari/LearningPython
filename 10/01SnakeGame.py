33c2c8bf64fc284e33f91e176e3a49a9e3c28417
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-08 23:46:12 +0545

    Initial

--- 10/01SnakeGame.py
+++ 10/01SnakeGame.py
@@ -2,3 +2,19 @@
 We all have played snake water gun game in our childhood. 
 If you haven't, google the rules of the game and write a python program capable of playing this gmae with the users.
 '''
+import random
+def GameStatus():
+    pass
+print("Computer Turn: Choose Snake(s),Water(w), Gun(g): ")
+
+com=random.randint(1,3)
+if com==1:
+    com="s"
+elif(com==2):
+    com="w"
+else:
+    com="g"
+
+you = input("Your Turn: Choose Snake(s),Water(w),Gun(g): ")
+you = you.toLowerCase(you)
+print(you)
