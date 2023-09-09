c2c0ef9c45f52706e133ae590215abd059ab4c27
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-09 18:57:41 +0545

    Initial

--- 11/04WorkingWIth.py
+++ 11/04WorkingWIth.py
@@ -1,2 +1,14 @@
 with open("WithFile.txt","a") as f:
     f.write("Hello, I am Using WIth to Open file")
+    
+with open("WithFile.txt","r") as file:
+    data = file.read()
+    print(data)
+
+
+with open("WithFile.txt","w") as f:
+    f.write("Hello, I am Using WIth to Open file")
+
+
+
+
