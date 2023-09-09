162a6d3ce281b140295808d0fbb408667e476aab
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-09 19:22:34 +0545

    Initial

--- 11/05ReadFiles.py
+++ 11/05ReadFiles.py
@@ -1,7 +1,7 @@
 with open("poems.txt") as f:
     data=f.read()
     print(data)
-    if("Twinkle" in data or "twinkle" in data):
+    if(("Twinkle" or "twinkle") in data):
         print("Twinkle is Presnet in FIle")
     else:
         print("Twinkle is not Present in File")
