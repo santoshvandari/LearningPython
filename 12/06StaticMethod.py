ffbe11ec90cbd652286cdbcacc5e672ee2e0536e
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-10 19:46:09 +0545

    Initial

---
+++ 12/06StaticMethod.py
@@ -0,0 +1,9 @@
+class Greeting:
+    # def Greet(self):
+        # print("Welcome to the World of Programming!!!")
+    @staticmethod
+    def Greet():
+        print(f"Welcome to the World of Programming!!!")
+
+greet = Greeting()
+greet.Greet() 
