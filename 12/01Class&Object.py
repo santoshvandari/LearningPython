d05e5525ac46ed8682f4e27bf09b925bbd655daf
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-10 17:30:28 +0545

    Initial

---
+++ 12/01Class&Object.py
@@ -0,0 +1,5 @@
+class Student:
+    name = "Santosh"
+
+student = Student()
+print(student.name)
---
+++ 12/02SumOfNumbers.py
@@ -0,0 +1,8 @@
+class Sum:
+    def SumNumbers(self):
+        return self.n1+self.n2
+
+s=Sum()
+s.n1=10
+s.n2=20
+print("Sum = ",s.SumNumbers())   
