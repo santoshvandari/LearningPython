2d323b6094fabccc69d5a72e254771446eb2d112
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-10 17:33:00 +0545

    Initial

---
+++ 12/03AskingUSerInfo.py
@@ -0,0 +1,8 @@
+class Person:
+    name=address=age=None
+    def setData(n,a,ag):
+        name=n
+        address=a
+        age=ag
+    def printData(self):
+        print(f,"Name : {self.name}\nAddress : {self.address}\nAge : {self.age}")
