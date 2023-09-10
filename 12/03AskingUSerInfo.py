38726cf3af725c341c085ef4a75edc7bf61224d7
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-10 17:37:16 +0545

    Initial

--- 12/03AskingUSerInfo.py
+++ 12/03AskingUSerInfo.py
@@ -1,13 +1,14 @@
 class Person:
-    name=address=age=None
-    def setData(n,a,ag):
-        name=n
-        address=a
-        age=ag
     def printData(self):
-        print(f,"Name : {self.name}\nAddress : {self.address}\nAge : {self.age}")
+        print(f"Name : {self.name}\nAddress : {self.address}\nAge : {self.age}")
 
 p1=Person()
 p2=Person()
-p1.setData("Santosh","Kanakai",23)
-p2.setData("Kiran","Birtamode",22)
+p1.name="Santosh"
+p1.address="Kanakai"
+p1.age=23
+p2.name="Krian"
+p2.address="Birtamode"
+p2.age=21
+p1.printData()
+p2.printData()
