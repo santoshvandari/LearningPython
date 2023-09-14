2780a2d526653b63fc72250d9e8c63c01a45b296
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-14 20:43:25 +0545

    Initial

--- 13/05SuperKeyword.py
+++ 13/05SuperKeyword.py
@@ -4,3 +4,14 @@ class Person:
         self.name=name
     def Display(self):
         print(f"Name of Person : {self.name}")
+
+class Employee(Person):
+    def __init__(self,name,comapny):
+        super(name)
+        self.company=comapny
+        print("Initilizing Employee....")
+        self.name=name
+    def Display(self):
+        super().Display()
+        print(f"Company of Employee : {self.company}")
+
