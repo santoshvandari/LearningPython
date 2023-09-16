9c057b640cd81eeafa4671d7a545194b6b51ba63
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-16 16:06:35 +0545

    Initial

---
+++ 13/09OperaterOverloading.py
@@ -0,0 +1,18 @@
+class Data:
+    def __init__(self,num):
+        self.number=num
+    def __add__(obj1,obj2):
+        return obj1.number+obj2.number
+    def __sub__(obj1,obj2):
+        return obj1.number-obj2.number
+    def __mul__(obj1,obj2):
+        return obj1.number*obj2.number
+    def __div__(obj1,obj2):
+        return obj1.number/obj2.number
+
+obj1=Data(10)
+obj2=Data(5)
+print(obj1+obj2)
+print(obj1-obj2)
+print(obj1*obj2)
+print(obj1/obj2)
