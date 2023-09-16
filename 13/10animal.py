053c71eb358caac6f8441f25343f2f71a34e9389
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-16 20:05:04 +0545

    Initial

--- 13/10animal.py
+++ 13/10animal.py
@@ -3,11 +3,20 @@ class Animal:
         self.name=name 
     
 class Pets(Animal):
-    def __init__(self,name,types):
+    def __init__(self,name,gender):
         super().__init__(name)
-        self.type=types
+        self.gender=gender
 
 class Dog(Pets):
-    def __init__(self,name,type,bark):
-        super().
+    def __init__(self,name,gender,bark):
+        super().__init__(name,gender)
+        self.bark=bark
+    def Display(self):
+        print(f"Name : {self.name}\nType : {self.gender}")
+    def Bark(self):
+        print(f"Barking : {self.bark}")
+
+dog = Dog("Jenny","Female","Bow Bow")
+dog.Display()
+dog.Bark()
 
