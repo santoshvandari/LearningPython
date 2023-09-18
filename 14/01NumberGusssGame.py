d72d6aa7ca2a8fe3fdca041db5cccb8e3e733f15
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-18 19:43:34 +0545

    Initial

--- 14/01NumberGusssGame.py
+++ 14/01NumberGusssGame.py
@@ -1,7 +1,12 @@
 import random
 randomNum= random.randint(10,20)
-print("Guess the Selected Number>>>")
-
-# userNum=int(input("Enter Your Guess Number: "))
-class Guess:
-    pass
+Guess=0
+GuessNumber=None
+while(GuessNumber==randomNum):
+    GuessNumber=int(input("Enter a Guess Number: "))
+    Guess+=1
+    if(GuessNumber==randomNum):
+        print(f"You have Guess the Correct Number in {Guess} Guesses!!!")
+    else:
+        if(GuessNumber>randomNum):
+            print("You Guessed Wrong Number! Please Guess Smaller Number.")
