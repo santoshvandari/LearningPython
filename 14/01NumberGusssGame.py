d8b70bb2224b60d700210aef0795425e6d4f4bda
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-18 20:02:03 +0545

    Initial

--- 14/01NumberGusssGame.py
+++ 14/01NumberGusssGame.py
@@ -6,16 +6,20 @@ randomNum= random.randint(1,100)
 # print(randomNum)
 Guess=0
 GuessNumber=None
+# Looping the Guessing Process Until the Guess Number is equal to Random Number.
 while(GuessNumber!=randomNum):
     GuessNumber=int(input("Enter a Guess Number: "))
     Guess+=1
+    # If the User Guess is Correct.
     if(GuessNumber==randomNum):
         print(f"You have Guess the Correct Number in {Guess} Guesses!!!")
+    # If the User Guess is not correct.
     else:
         if(GuessNumber>randomNum):
             print("You Guessed Wrong Number! Please Guess Smaller Number.")
         else:
             print("You Guessed Wrong Number! Please Guess Larger Number.")
+# Reading the Highest Score File of the Previously set.
 with open("HighestScore.txt","r") as f:
     highScore=int(f.read())
 if(highScore>Guess):
