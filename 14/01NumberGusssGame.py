c3ede7bdb25208822b908db4dd81f515aaed7448
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-18 19:53:43 +0545

    Initial

--- 14/01NumberGusssGame.py
+++ 14/01NumberGusssGame.py
@@ -1,21 +1,21 @@
-import random
-randomNum= random.randint(1,100)
-print(randomNum)
+# import random
+# randomNum= random.randint(1,100)
+# print(randomNum)
 Guess=0
-GuessNumber=None
-while(GuessNumber!=randomNum):
-    GuessNumber=int(input("Enter a Guess Number: "))
-    Guess+=1
-    if(GuessNumber==randomNum):
-        print(f"You have Guess the Correct Number in {Guess} Guesses!!!")
-    else:
-        if(GuessNumber>randomNum):
-            print("You Guessed Wrong Number! Please Guess Smaller Number.")
-        else:
-            print("You Guessed Wrong Number! Please Guess Larger Number.")
+# GuessNumber=None
+# while(GuessNumber!=randomNum):
+#     GuessNumber=int(input("Enter a Guess Number: "))
+#     Guess+=1
+#     if(GuessNumber==randomNum):
+#         print(f"You have Guess the Correct Number in {Guess} Guesses!!!")
+#     else:
+#         if(GuessNumber>randomNum):
+#             print("You Guessed Wrong Number! Please Guess Smaller Number.")
+#         else:
+#             print("You Guessed Wrong Number! Please Guess Larger Number.")
 with open("HighestScore.txt","r") as f:
     highScore=int(f.read())
-if(highScore>GuessNumber):
+if(highScore>Guess):
     print("Congrats!!! You Have Broken Highest Score!")
     with open("HighestScore.txt","w") as f:
         f.write(str(GuessNumber))
