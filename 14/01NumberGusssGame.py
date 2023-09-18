94490c3bd4f9bfdee247afc222286905c6b4a41c
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-18 19:46:03 +0545

    Initial

--- 14/01NumberGusssGame.py
+++ 14/01NumberGusssGame.py
@@ -3,7 +3,7 @@ randomNum= random.randint(1,100)
 print(randomNum)
 Guess=0
 GuessNumber=None
-while(GuessNumber==randomNum):
+while(GuessNumber!=randomNum):
     GuessNumber=int(input("Enter a Guess Number: "))
     Guess+=1
     if(GuessNumber==randomNum):
