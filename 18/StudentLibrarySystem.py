ebfb9351b28dac4a4aa9906027bb98968c72b346
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-19 19:50:03 +0545

    Initial

---
+++ 18/StudentLibrarySystem.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self,books):
+        self.books=books
+    def DisplayAvailableBooks(self):
+        print("The Available Books are: ")
+        for i in self.books:
+            print("\t - ",i)
+
+class Student:
+    pass
+
+if __name__=="__main__":
+    pass
