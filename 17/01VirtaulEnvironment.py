026a868e405dbd4fb9345461e180fecae743af14
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-19 15:56:15 +0545

    Initial

---
+++ 17/01VirtaulEnvironment.py
@@ -0,0 +1,8 @@
+'''
+"A virtual environment is a Python environment such that the Python interpreter, libraries and scripts installed into it are isolated from those installed in other virtual environments, and (by default) any libraries installed in a “system” Python, i.e., one which is installed as part of your operating system"
+'''
+# commands to create Virtual environment 
+'''
+step 1: 
+    Install Virtual Environment using the Command: 
+'''
