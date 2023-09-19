b6594a627b91f4d87187f425de0fa186e648b220
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-19 10:25:43 +0545

    Initial

--- 15/01TryCatch.py
+++ 15/01TryCatch.py
@@ -6,4 +6,7 @@ while(True):
     try:
         a=int(a)
         if(a>10):
-            print("Yes input Vaue ")
+            print("Yes input Vaue is Greater than 10.....")
+    except Exception as e:
+        print(f"You have entered Wrong. Error: {e}")
+print("Thank You For Playing Game....")
