9c2f1f5b13334f6ef74aba9ba7839e7e03eb7ae2
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-19 11:13:20 +0545

    Initial

--- 15/06GlobalVariable.py
+++ 15/06GlobalVariable.py
@@ -1,10 +1,14 @@
 a=100
+b=50
 def function():
     global a
     # if we use global keyword then it will access a global variable which was already created.
     # IF we don't use Global keyword then it will create new local variable
     print(a)
     a=50
+    b=10
     print(a)
+    print(b)
 function()
 print(a)
+print(b)
