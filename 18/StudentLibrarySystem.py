a43cd8e26cf1543b2cdb7d80d4cd359aaf5274e3
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-19 19:52:06 +0545

    Initial

--- 18/StudentLibrarySystem.py
+++ 18/StudentLibrarySystem.py
@@ -5,6 +5,10 @@ class Student:
         print("The Available Books are: ")
         for i in self.books:
             print("\t - ",i)
+    def IssueBook(self,BookName):
+        if(BookName in self.books):
+            self.books.remove(BookName)
+            print(f"The {BookName} Book is Available and issued to You.")
 
 class Student:
     pass
