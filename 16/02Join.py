d01798c395c5418545d4ae495db82209ee5a9743
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-19 15:46:22 +0545

    Initial

--- 16/02Join.py
+++ 16/02Join.py
@@ -1,3 +1,8 @@
+l
 list=["Santosh","Aditya","Krishna","Manoj","Uttam"]
 joinedItems="My Friends are: "+", ".join(list)
+print(joinedItems)
+
+tuple=("Santosh","Aditya","Krishna","Manoj","Uttam")
+joinedItems="My Friends are: "+", ".join(tuple)
 print(joinedItems)
