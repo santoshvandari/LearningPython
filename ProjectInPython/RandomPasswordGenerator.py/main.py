59c3c7a059f3e338482f185eab81715e63f9e1f0
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-20 20:29:48 +0545

    Initial

--- ProjectInPython/RandomPasswordGenerator.py/main.py
+++ ProjectInPython/RandomPasswordGenerator.py/main.py
@@ -1,8 +1,6 @@
-# importing the necessary module  
-import random                       # importing the random module  
+import random  
  
 def GeneratePassword(Type,length):    
-    # defining the list of characters that will be used to generate the password  
     PasswordCombination=None
     if(Type==1):
         PasswordCombination="abcdefghijklmnopqrstuvwxyz"
@@ -14,19 +12,11 @@ def GeneratePassword(Type,length):
         PasswordCombination="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
     elif(Type==5):
         PasswordCombination="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()<>?;"
-
-    # defining an empty password string  
-    GeneratePassword = ""  
-  
-    # creating a loop to randomly select the character from the list and insert it in the password string upto the given length  
+    GeneratePassword = ""   
     for i in range(len):  
-        # using the random.choice() method to select a random character from the list and inserting it in the password string  
         GeneratePassword = GeneratePassword + random.choice(PasswordCombination)  
-      
-    # returning the generated password string  
     return GeneratePassword 
 
-# main function  
 if __name__ == "__main__":  
     while(True):
         welcomeDisplay='''\n********** Random Password Generator **********
