aab41c04030166ebfff6f93815a6328f7bf3b9d0
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-21 11:06:02 +0545

    Initial

--- ProjectInPython/Calenders/main.py
+++ ProjectInPython/Calenders/main.py
@@ -1,6 +1,9 @@
+# Importing the Calendar Module 
 import calendar
+# Using the Try-Except for Handeling Errors
 try:
     year=int(input("Enter a Year: "))
+    # Creating the Calander 
     disp=calendar.calendar(year)
     print(disp)
 except Exception as e:
