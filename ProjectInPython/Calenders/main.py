61bc891c51c840119fed743954ea50a077b21a10
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-21 11:03:20 +0545

    Initial

--- ProjectInPython/Calenders/main.py
+++ ProjectInPython/Calenders/main.py
@@ -1,8 +1,6 @@
 import calendar
 try:
-    # year=int(input("Enter a Year: "))
-    year=2020
+    year=int(input("Enter a Year: "))
     disp=calendar.calendar(year)
-    print(disp)
 except Exception as e:
     print("Invalid Entry: Error: ",e)
