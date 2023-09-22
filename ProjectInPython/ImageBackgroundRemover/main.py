12365b1ede06a7e6e1c15ba0366527b0397d3719
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-22 21:38:38 +0545

    Initial

---
+++ ProjectInPython/ImageBackgroundRemover/main.py
@@ -0,0 +1,10 @@
+from rembg import remove
+
+input_path = 'img/input.png'
+output_path = 'img/output.png'
+
+with open(input_path, 'rb') as i:
+    with open(output_path, 'wb') as o:
+        input = i.read()
+        output = remove(input)
+        o.write(output)
