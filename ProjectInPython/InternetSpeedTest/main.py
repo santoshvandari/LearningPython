73a1d6da9eb68ece7e6cef8559f45cf0f9944625
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-07 20:05:13 +0545

    Initial

--- ProjectInPython/InternetSpeedTest/main.py
+++ ProjectInPython/InternetSpeedTest/main.py
@@ -2,15 +2,14 @@ import speedtest
 
 def MB(bytes):
     return int(bytes/(1024*1024))
-# def Speed():
-    # Speed()
+def Speed():
+    download = MB(st.download())
+    upload = MB(st.upload())
+    ping = st.results.ping
+    print(f"Download : {download} MB/s\tUpload : {upload} MB/s\tPing : {ping} ms")
+    Speed()
 if __name__=="__main__":
     st = speedtest.Speedtest()
     print("Internet Speed")
-    download = MB(st.download())
-    upload = MB(st.upload())
-    # ping = st.results.ping
-    ping="Hello"
-    print(f"Download : {download} MB/s\tUpload : {upload} MB/s\tPing : {ping} ms")
-    # Speed()
+    Speed()
 
