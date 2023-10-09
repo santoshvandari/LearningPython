de2a839026df4c67e6794bad6509bdc747c4dd8c
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-09 19:32:16 +0545

    Initial

--- ProjectInPython/ZipFileCracker/main.py
+++ ProjectInPython/ZipFileCracker/main.py
@@ -1,28 +1,27 @@
-import zipfile
+# import zipfile
+
+# def crack_password(password_list, obj):
+#     # tracking line no. at which password is found
+#     idx = 0
+ 
+#     # open file in read byte mode only as "rockyou.txt"
+#     # file contains some special characters and hence
+#     # UnicodeDecodeError will be generated
+#     with open(password_list, 'rb') as file:
+#         for line in file:
+#             for word in line.split():
+#                 try:
+#                     idx += 1
+#                     obj.extractall(pwd=word)
+#                     print("Password found at line", idx)
+#                     print("Password is", word.decode())
+#                     return True
+#                 except:
+#                     continue
+#     return False
  
  
-def crack_password(password_list, obj):
-    # tracking line no. at which password is found
-    idx = 0
- 
-    # open file in read byte mode only as "rockyou.txt"
-    # file contains some special characters and hence
-    # UnicodeDecodeError will be generated
-    with open(password_list, 'rb') as file:
-        for line in file:
-            for word in line.split():
-                try:
-                    idx += 1
-                    obj.extractall(pwd=word)
-                    print("Password found at line", idx)
-                    print("Password is", word.decode())
-                    return True
-                except:
-                    continue
-    return False
- 
- 
-password_list = "rockyou.txt"
+# password_list = "rockyou.txt"
  
 zip_file = "gfg.zip"
  
@@ -30,18 +29,18 @@ zip_file = "gfg.zip"
 obj = zipfile.ZipFile(zip_file)
  
 # count of number of words present in file
-cnt = len(list(open(password_list, "rb")))
+# cnt = len(list(open(password_list, "rb")))
  
-print("There are total", cnt,
-      "number of passwords to test")
+# print("There are total", cnt,
+#       "number of passwords to test")
  
-if crack_password(password_list, obj) == False:
-    print("Password not found in this file")
+# if crack_password(password_list, obj) == False:
+#     print("Password not found in this file")
 
 
 
 
-    import zipfile
+import zipfile
 import itertools
 
 def crack_password(zip_file, max_length=8):
@@ -62,7 +61,7 @@ def crack_password(zip_file, max_length=8):
     
     return None
 
-zip_file = "gfg.zip"
+zip_file = "file.zip"
 result = crack_password(zip_file)
 
 if result is None:
