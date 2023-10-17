7b0df838513c0da7554fe7ddd3a4afb6ee53c0b1
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-17 09:09:40 +0545

    Initial

---
+++ ProjectInPython/ZipFilePasswordCracker/main.py
@@ -0,0 +1,12 @@
+# Write a Program in Python that will crack the password of a zip file.
+# The program should take the name of the zip file as an argument and
+# should try different passwords to crack the zip file.
+# The program should print the password that is used to crack the zip file.
+# If the password is not found, the program should print a message that
+# the password is not found and exit.
+# The program should be able to crack the password in a reasonable time.
+
+import zipfile
+import sys
+import time
+
