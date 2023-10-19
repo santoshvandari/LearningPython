291963cfd7954e8497ca2e741841bb13ef0f2a9e
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-19 09:12:11 +0545

    Initial

--- ProjectInPython/PythonServer/main.py
+++ ProjectInPython/PythonServer/main.py
@@ -1,41 +1,58 @@
 import http.server
 import socketserver
-import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import os
+import webbrowser
 
 # Set the port you want the server to run on
 port = 8080
 
-# Specify the directory where your webpage file is located
+# Get the current directory where the script is located
 web_directory = os.getcwd()
 
 # Create a simple HTTP server
 handler = http.server.SimpleHTTPRequestHandler
+httpd = socketserver.TCPServer(("", port), handler)
 
-# Create the server with the specified port
-with socketserver.TCPServer(("", port), handler) as httpd:
-    print(f"Serving on port {port}")
+# Flag to track if the server is running
+server_running = False
 
-    # Define a custom event handler to watch for changes in the directory
-    class MyHandler(FileSystemEventHandler):
-        def on_modified(self, event):
-            if event.is_directory:
-                return
-            print(f'Reloading due to change in: {event.src_path}')
-            httpd.shutdown()  # Shutdown the server
-
-    # Start watching for file changes
-    event_handler = MyHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path=web_directory, recursive=True)
-    observer.start()
-
-    try:
+def start_server():
+    global server_running
+    if not server_running:
+        print(f"Serving on port {port}")
+        # Open the default web browser
+        webbrowser.open(f"http://localhost:{port}")
         # Start the server
         httpd.serve_forever()
-    except KeyboardInterrupt:
-        observer.stop()
+        server_running = True
 
-# Join the observer thread
-observer.join()
+# Define a custom event handler to watch for changes in the directory
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.is_directory:
+            return
+        print(f'Reloading due to change in: {event.src_path}')
+        
+        # If the server is running, shut it down
+        if server_running:
+            httpd.shutdown()
+            httpd.server_close()
+        
+        # Recreate and start the server
+        start_server()
+
+# Start the server
+start_server()
+
+# Define an observer to watch for file changes
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, path=web_directory, recursive=True)
+observer.start()
+
+try:
+    observer.join()  # Join the observer thread
+except KeyboardInterrupt:
+    observer.stop()
