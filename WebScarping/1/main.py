e65a21da76a0340035d6c625157dd6d26ee18db8
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-02 09:33:04 +0545

    Initial

--- WebScarping/1/main.py
+++ WebScarping/1/main.py
@@ -1,9 +1,1 @@
-class Hello:
-    def __init__(self,msg):
-        print(msg)
-        self.name = msg
-    def Greet(self):
-        print(self.name)
-
-msg=Hello("Hello Santosh")
-msg.Greet()
+# Learning Web Scarping Using Scarpy 
