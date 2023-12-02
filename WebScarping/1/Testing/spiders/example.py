c31211b947c4808af7d35313df49dbe125b5cc12
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-02 19:40:23 +0545

    Initial

--- WebScarping/1/Testing/spiders/example.py
+++ WebScarping/1/Testing/spiders/example.py
@@ -7,14 +7,25 @@ class ExampleSpider(scrapy.Spider):
     name = "example"
     def start_requests(self):
         urls = [
-            "http://books.toscrape.com/",
+            "http://books.toscrape.com/catalogue/page-1.html",
+            "http://books.toscrape.com/catalogue/page-2.html",
+            "http://books.toscrape.com/catalogue/page-3.html",
+            "http://books.toscrape.com/catalogue/page-4.html",
+            "http://books.toscrape.com/catalogue/page-5.html",
+            "http://books.toscrape.com/catalogue/page-6.html",
+            "http://books.toscrape.com/catalogue/page-7.html",
+            "http://books.toscrape.com/catalogue/page-8.html",
+            "http://books.toscrape.com/catalogue/page-9.html",
+            "http://books.toscrape.com/catalogue/page-10.html",
+
+            
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        # page = response.url.split("/")[-2]
-        filename = f"booksdata.html"
+        page = response.url.split("/")[-1].split(".")[0]
+        filename = f"book-{page}.html"
         Path(filename).write_bytes(response.body)
         self.log(f"Saved file {filename}")
         bookcards=response.css(".product_pod")
