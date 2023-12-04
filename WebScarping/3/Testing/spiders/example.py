d3233591fdc635849fc4ea2db31359043bbc1397
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-04 13:16:15 +0545

    Initial

--- WebScarping/3/Testing/spiders/example.py
+++ WebScarping/3/Testing/spiders/example.py
@@ -16,7 +16,7 @@ class ExampleSpider(scrapy.Spider):
                                 playwright_include_page=True,
                                 playwright_page_methods=[
                                     # This where we can implement scrolling if we want
-                                    PageMethod('wait_for_selector', 'table.table')
+                                    PageMethod('wait_for_selector', 'table#DataTables_Table_0')
                                 ]
                             )
                             )
@@ -24,7 +24,7 @@ class ExampleSpider(scrapy.Spider):
     def parse(self, response):
         tabledata= response.css('table.table tbody tr')
         for data in tabledata:
-            symbol = data.css('td:nth-child(2) a::text').get()
-            ltp = data.css('td:nth-child(10) span::text').get()
+            symbol = data.css('td:nth-child(1) a::text').get()
+            ltp = data.css('td:nth-child(9) span::text').get()
             with open('nepsedata.txt', 'a') as f:
                 f.write(f'Symbol: {symbol}\nLast Price: {ltp}\n\n\n')
