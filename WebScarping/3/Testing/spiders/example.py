bb365ff99e315010071cfd2162a6ffebdca3d74c
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-04 17:04:53 +0545

    intitial

--- WebScarping/3/Testing/spiders/example.py
+++ WebScarping/3/Testing/spiders/example.py
@@ -6,25 +6,28 @@ from pathlib import Path
 
 class ExampleSpider(scrapy.Spider):
     name = 'example'
-    allowed_domains = ['https://www.nepsealpha.com/nepse-data']
-    start_urls = 'https://www.nepsealpha.com/nepse-data/'
+    allowed_domains = ['www.sharesansar.com']
+    start_urls = 'https://www.sharesansar.com/live-trading'
 
     def start_requests(self):
-        yield scrapy.Request(self.start_urls,
+        yield scrapy.Request('https://www.sharesansar.com/live-trading',
                             meta=dict(
                                 playwright=True,
                                 playwright_include_page=True,
                                 playwright_page_methods=[
                                     # This where we can implement scrolling if we want
-                                    PageMethod('wait_for_selector', 'table#DataTables_Table_0')
+                                    PageMethod('wait_for_selector', 'table#headFixed'),
                                 ]
                             )
                             )
 
     def parse(self, response):
-        tabledata= response.css('table.table tbody tr')
+        # with open('nepsedata.html', 'w') as f:
+        #     f.write(response.body.decode('utf-8'))
+
+        tabledata= response.css('table#headFixed tbody tr')
         for data in tabledata:
-            symbol = data.css('td:nth-child(1) a::text').get()
-            ltp = data.css('td:nth-child(9) span::text').get()
+            symbol = data.css('td:nth-child(2) a::text').get()
+            ltp = data.css('td:nth-child(3)::text').get()
             with open('nepsedata.txt', 'a') as f:
-                f.write(f'Symbol: {symbol}\nLast Price: {ltp}\n\n\n')
+                f.write(f'Symbol: {symbol}\nLast Price: {ltp} \n\n\n')
