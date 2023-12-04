99c0d9a45f13f81873ed7e732ccf9715a2f9cbc6
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-04 11:39:59 +0545

    Initial

--- WebScarping/3/Testing/spiders/example.py
+++ WebScarping/3/Testing/spiders/example.py
@@ -1,4 +1,7 @@
 import scrapy
+from scrapy_playwright.page import PageMethod
+from pathlib import Path
+
 
 
 class ExampleSpider(scrapy.Spider):
@@ -7,7 +10,21 @@ class ExampleSpider(scrapy.Spider):
     start_urls = ['http://https://www.nepsealpha.com/nepse-data/']
 
     def start_requests(self):
-        return super().start_requests()
+       yield scrapy.Request(self.start_urls,
+                            meta=dict(
+                                playwright=True,
+                                playwright_include_page=True,
+                                playwright_page_methods=[
+                                    # This where we can implement scrolling if we want
+                                    PageMethod('wait_for_selector', 'table.table')
+                                ]
+                            )
+                            )
 
     def parse(self, response):
-        pass
+        tabledata= response.css('table.table tbody tr')
+        for data in tabledata:
+            symbol = data.css('td:nth-child(2) a::text').get()
+            ltp = data.css('td:nth-child(10) span::text').get()
+            with open('nepsedata.txt', 'a') as f:
+                f.write(f'Symbol: {symbol}\nLast Price: {ltp}\n\n\n')
