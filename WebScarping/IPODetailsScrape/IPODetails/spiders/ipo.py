1e4dff45f51966881807e4bc9f8f0067f47f8e25
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-11 13:30:02 +0545

    Initial

--- WebScarping/IPODetailsScrape/IPODetails/spiders/ipo.py
+++ WebScarping/IPODetailsScrape/IPODetails/spiders/ipo.py
@@ -20,16 +20,17 @@ class IpoSpider(scrapy.Spider):
         # for bookcard in bookcards:
         # for row in tablebody:
         for row in tablebody:
-            print("**********")
-            print(row)
-            symbol = row.css("td:nth-child(1):text").get()
-            fullname= row.css("td:nth-child(2):text").get()
-            totalunit= row.css("td:nth-child(3):text").get()
-            openprice= row.css("td:nth-child(5):text").get()
-            closeprice= row.css("td:nth-child(6):text").get()
+            # print("**********")
+            # print(row)
+            symbol = row.css("td:nth-child(1)::text").get()
+            fullname= row.css("td:nth-child(2)::text").get()
+            totalunit= row.css("td:nth-child(3)::text").get()
+            openprice= row.css("td:nth-child(5)::text").get()
+            closeprice= row.css("td:nth-child(6)::text").get()
             # title = table.css("h3 a::attr(title)").get()
             # price = table.css(".price_color::text").get()
             # rating = table.css(".star-rating::attr(class)").get().split(" ")[-1]+" stars"
-            file.write(f"Symbol: {symbol}\nFull Name: {fullname}\nTotal Unit: {totalunit}\nOpen Price: {openprice}\nClose Price: {closeprice}\n\n\n")
+            if(openprice is not None and closeprice is not None):
+                file.write(f"Symbol: {symbol}\nFull Name: {fullname}\nTotal Unit: {totalunit}\nOpen Price: {openprice}\nClose Price: {closeprice}\n\n\n")
         file.close()
 
