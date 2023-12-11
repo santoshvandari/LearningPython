4b07cbaa51cfb48ad6345bc67cbbc8871589d616
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-11 13:18:23 +0545

    Initial

--- WebScarping/IPODetailsScrape/IPODetails/spiders/ipo.py
+++ WebScarping/IPODetailsScrape/IPODetails/spiders/ipo.py
@@ -16,14 +16,17 @@ class IpoSpider(scrapy.Spider):
         self.log(f"Saved file {filename}")
         table= response.css("#tablepress-UpcomingIPOs")
         tablebody= table.css("tbody tr")
-        file = open("bookdata", "a")
+        file = open("bookdata.txt", "a")
         # for bookcard in bookcards:
+        # for row in tablebody:
         for row in tablebody:
-            symbol = row.css("td:nth-child(1)")
-            fullname= row.css("td:nth-child(2)")
-            totalunit= row.css("td:nth-child(3)")
-            openprice= row.css("td:nth-child(5)")
-            closeprice= row.css("td:nth-child(6)")
+            print("**********")
+            print(row)
+            symbol = row.css("td:nth-child(1):text").get()
+            fullname= row.css("td:nth-child(2):text").get()
+            totalunit= row.css("td:nth-child(3):text").get()
+            openprice= row.css("td:nth-child(5):text").get()
+            closeprice= row.css("td:nth-child(6):text").get()
             # title = table.css("h3 a::attr(title)").get()
             # price = table.css(".price_color::text").get()
             # rating = table.css(".star-rating::attr(class)").get().split(" ")[-1]+" stars"
