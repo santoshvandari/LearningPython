8f21efdd15aa98606860bb18ff1f29fd20ad1049
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-11 11:04:49 +0545

    Initial

--- WebScarping/IPODetailsScrape/IPODetails/spiders/ipo.py
+++ WebScarping/IPODetailsScrape/IPODetails/spiders/ipo.py
@@ -1,4 +1,5 @@
 import scrapy
+from pathlib import Path
 
 
 class IpoSpider(scrapy.Spider):
@@ -10,4 +11,22 @@ class IpoSpider(scrapy.Spider):
         yield scrapy.Request(url=self.url)
 
     def parse(self, response):
-        pass
+        filename = "data.html"
+        Path(filename).write_bytes(response.body)
+        self.log(f"Saved file {filename}")
+        table= response.css("#tablepress-UpcomingIPOs")
+        tablebody= table.css("tbody tr")
+        file = open("bookdata", "a")
+        # for bookcard in bookcards:
+        for row in tablebody:
+            symbol = row.css("td:nth-child(1)")
+            fullname= row.css("td:nth-child(2)")
+            totalunit= row.css("td:nth-child(3)")
+            openprice= row.css("td:nth-child(5)")
+            closeprice= row.css("td:nth-child(6)")
+            # title = table.css("h3 a::attr(title)").get()
+            # price = table.css(".price_color::text").get()
+            # rating = table.css(".star-rating::attr(class)").get().split(" ")[-1]+" stars"
+            file.write(f"Symbol: {symbol}\nFull Name: {fullname}\nTotal Unit: {totalunit}\nOpen Price: {openprice}\nClose Price: {closeprice}\n\n\n")
+        file.close()
+
