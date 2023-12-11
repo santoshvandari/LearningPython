06fdca79a70e9a159b076609a4f87ae2874ed474
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-11 10:54:31 +0545

    Initial

--- WebScarping/IPODetailsScrape/IPODetails/spiders/ipo.py
+++ WebScarping/IPODetailsScrape/IPODetails/spiders/ipo.py
@@ -3,8 +3,11 @@ import scrapy
 
 class IpoSpider(scrapy.Spider):
     name = 'ipo'
-    allowed_domains = ['https://investornepal.com/upcoming-ipo/']
-    start_urls = ['http://https://investornepal.com/upcoming-ipo//']
+    # allowed_domains = ['https://investornepal.com/upcoming-ipo/']
+    # start_urls = ['http://https://investornepal.com/upcoming-ipo//']
+    url='https://investornepal.com/upcoming-ipo/'
+    def start_requests(self):
+        yield scrapy.Request(url=self.url)
 
     def parse(self, response):
         pass
